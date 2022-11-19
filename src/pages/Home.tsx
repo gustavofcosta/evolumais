36647d31a5b4820703e6602f2cb0f23f43bad37d
gustavofcosta <gfcdesenvolvimento@gmail.com>
2022-11-19 20:25:15 -0300

    removendo scrol superior

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,26 +6,14 @@ import Header from "../components/landpage/Header";
 import Main from "../components/landpage/Main";
 
 const Home = () => {
-  const { scrollYProgress } = useScroll();
-
   return (
-    <motion.div
-      initial={{ scaleY: 0 }}
-      animate={{ scaleY: 1 }}
-      exit={{ scaleY: 0 }}
-      transition={{ duration: 0.5 }}
-      className="overflow-x-hidden"
-    >
-      <motion.div
-        className="fixed z-50 top-0 left-0 right-0 h-2 bg-secondary_500 origin-left "
-        style={{ scaleX: scrollYProgress }}
-      />
+    <div>
       <Navbar />
 
       <Header />
       <Main />
       <Footer />
-    </motion.div>
+    </div>
   );
 };
 
